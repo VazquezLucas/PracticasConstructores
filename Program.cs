8c35ec02bc11180c14766377f5ad5dd111c466b8
Lucas Vazquez <vazquezlucasmatias2001@gmail.com>
2023-03-02 15:42:16 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PracticasConstructores
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+
+    class Coche
+    {
+
+        private int ruedas;
+
+        private double largo;
+
+        private double ancho;
+
+        private bool climatizador;
+
+        private String tapiceria;
+
+
+
+    }
+}
